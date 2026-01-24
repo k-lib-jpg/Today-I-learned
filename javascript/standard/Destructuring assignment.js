2568d686631fbb3637b5d3154f7f2416c58f6312
Kyon001 <k-liberty080822@outlook.com>
2026-01-24 20:58:54 +0900

    Fix comment for destructuring assignment example

--- javascript/standard/Destructuring assignment.js
+++ javascript/standard/Destructuring assignment.js
@@ -12,7 +12,7 @@ const message1 = `名前は${name}です。年齢は${age}です。`;
 const myProfile = ['じゃけぇ', 31];
 const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}です。`;
 
-//分割代入4
+//分割代入3
 const [name, age] = myProfile;
 const message4 = `名前は${name}です。年齢は${age}です。`;
 

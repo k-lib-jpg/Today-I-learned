7951464dd049e72f165604db15203f59fc3a1048
Kyon001 <k-liberty080822@outlook.com>
2026-02-07 22:42:05 +0900

    Create generics.ts

---
+++ typescript/standard/generics.ts
@@ -0,0 +1,18 @@
+const Example = () => {
+  const repeatStr = (value: string, times: number): string[] => {
+    return new Array(times).fill(value);
+  }
+
+  const repeatNum = (value: number, times: number): number[] => {
+    return new Array(times).fill(value);
+  }
+  const repeat = <T>(value: T, times: number): T[] => {
+    return new Array(times).fill(value);
+  }
+  
+
+  const numArry = repeat<number>(10, 3);
+  const strArry = repeat<string>("hello", 3);
+};
+
+export default Example;

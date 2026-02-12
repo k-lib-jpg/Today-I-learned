9843c26c034d830adad2fc48532ba7478d848ca7
Kyon001 <k-liberty080822@outlook.com>
2026-02-13 00:06:47 +0900

    Update select.sql

--- SQL/select.sql
+++ SQL/select.sql
@@ -1,6 +1,15 @@
 SELECT <列名>
   FROM <テーブル名>;
 
-
 SELECT *
   FROM <テーブル名>;
+
+SELECT <列名> AS <新列名>
+  FROM <テーブル名>;
+
+SELECT DISTINCT <列名>
+  FROM <テーブル名>;
+
+SELECT ＜列名>,
+  FROM <テーブル名>
+ WHERE <条件式>;

3e1a4672b45b9cb531b29b98ae99ed18e96095c5
Kyon001 <k-liberty080822@outlook.com>
2026-02-20 23:27:06 +0900

    Update arithmetic-operators.sql

--- SQL/arithmetic-operators.sql
+++ SQL/arithmetic-operators.sql
@@ -3,3 +3,7 @@ SELECT hanbai_tanka * 2 AS "hanbai_tanka_x2"
   FROM Shohin;
 
 /* +足し算、-引き算、*掛け算、/割り算 */
+
+SELECT shohin_mei, shohin_bunrui, hanbai_tanka
+  FROM Shohin
+ WHERE NOT hanbai_tanka >= 1000;

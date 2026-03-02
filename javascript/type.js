17a46ecd732924c6329464cd5a63f72fe07943f9
Kyon001 <k-liberty080822@outlook.com>
2026-03-02 22:24:41 +0900

    Create type.js

---
+++ javascript/type.js
@@ -0,0 +1,13 @@
+//primitive
+console.log(typeof true);// => "boolean"
+console.log(typeof 42); // => "number"
+console.log(typeof 9007199254740992n); // => "bigint"
+console.log(typeof "JavaScript"); // => "string"
+console.log(typeof Symbol("シンボル"));// => "symbol"
+console.log(typeof undefined); // => "undefined"
+
+//object
+console.log(typeof null); // => "object"
+console.log(typeof ["配列"]); // => "object"
+console.log(typeof { "key": "value" }); // => "object"
+console.log(typeof function() {}); // => "function"

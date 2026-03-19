6b17164558c0a01206fbe2c0ba11cee5d46ad14b
Kyon001 <k-liberty080822@outlook.com>
2026-03-19 22:44:56 +0900

    Create class.js

---
+++ javascript/standard/class.js
@@ -0,0 +1,16 @@
+// クラスの定義
+
+class MyClass {
+    constructor() {
+        // コンストラクタ関数の処理
+        // インスタンス化されるときに自動的に呼び出される
+    }
+}
+
+const MyClass = class MyClass {
+    constructor() {}
+};
+
+const AnonymousClass = class {
+    constructor() {}
+};

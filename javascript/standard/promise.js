b3dc0999b3025a7ef2c47f112c557b12cbed5346
Kyon001 <k-liberty080822@outlook.com>
2026-03-20 22:21:32 +0900

    Update promise.js

--- javascript/standard/promise.js
+++ javascript/standard/promise.js
@@ -1,7 +1,23 @@
-// 非同期処理（promise)
+// 非同期処理
+// settimeout
+function taskA() {
+    console.log("タスクAを実行 at " + Date.now());
+}
+function taskB() {
+    console.log("タスクBを実行 at " + Date.now());
+}
+function taskAsync() {
+    console.log("非同期のタスクを実行 at " + Date.now());
+}
 
+taskA();
+setTimeout(() => {
+    taskAsync();
+}, 1000);
+taskB();
+
+// promise
 let a = 0;
-
 new Promise((resolve, reject) => {
     setTimeout(() => {
       a=1;

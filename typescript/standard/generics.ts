d60093d0e68cda4a88d3aa3d52b2e1a5ee9aae6d
Kyon001 <k-liberty080822@outlook.com>
2026-04-13 21:51:32 +0900

    Introduce generic repeat function for values
    Added a generic function to simplify repeated value creation.

--- typescript/standard/generics.ts
+++ typescript/standard/generics.ts
@@ -1,3 +1,4 @@
+// ジェネリクスを使わず型注釈された関数
 const Example = () => {
   const repeatStr = (value: string, times: number): string[] => {
     return new Array(times).fill(value);
@@ -6,6 +7,8 @@ const Example = () => {
   const repeatNum = (value: number, times: number): number[] => {
     return new Array(times).fill(value);
   }
+
+  // ジェネリクスを使用すると型注釈された関数を簡単に量産できる
   const repeat = <T>(value: T, times: number): T[] => {
     return new Array(times).fill(value);
   }

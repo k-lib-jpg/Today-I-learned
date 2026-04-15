ee0f7fffaa60c8927858c148001af96d66beb442
Kyon001 <k-liberty080822@outlook.com>
2026-04-15 23:12:22 +0900

    Replace Direction enum with ZeroOrOne enum

--- typescript/standard/enum.ts
+++ typescript/standard/enum.ts
@@ -1,18 +1,16 @@
 //数値や文字列をenumを使うことでまとめることができる
 
 #数値列挙 enunm + 宣言したい変数
-enum Direction {
- Up,    //(enum member) Direction.Up = 0
- Down,  //(enum member) Direction.Down = 1
- Left,  //(enum member) Direction.Left = 2
- Right, //(enum member) Direction.Right = 3
- }
- const left = Dirction.Left // const left: Direction.Left
+enum ZeroOrOne {
+  Zero = 0,
+  One = 1,
+}
+const zeroOrOne: ZeroOrOne = 0; 
 
  #文字列列挙
 
 enum Ports {
   USER_SERVICE = "8800",
   REGISTER_SERVICE = "8081",
-  MEDIA_SERVICE = "8888
+  MEDIA_SERVICE = "8888"
 }

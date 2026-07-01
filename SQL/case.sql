3338b414a8c2c41b4b32073a76298987c68fb601
Kyon001 <k-liberty080822@outlook.com>
2026-07-01 21:37:41 +0900

    Add case expression examples to case.sql
    Added examples of simple and searched case expressions in SQL.

--- SQL/case.sql
+++ SQL/case.sql
@@ -4,3 +4,20 @@ SELECT shohin_mei,
             WHEN <評価式> THEN <式>
             ELSE <式>
        END
+--単純case式　例
+SELECT shohin_mei,
+       CASE WHEN '衣服'         THEN 'A：' || shohin_bunrui
+            WHEN '事務用品'     THEN 'B：' || shohin_bunrui
+            WHEN 'キッチン用品' THEN 'C：' || shohin_bunrui
+            ELSE NULL
+       END AS abc_shohin_bunrui
+  FROM Shohin;
+       
+--検索case式　例
+SELECT shohin_mei,
+       CASE WHEN shohin_bunrui = '衣服'         THEN 'A：' || shohin_bunrui
+            WHEN shohin_bunrui = '事務用品'     THEN 'B：' || shohin_bunrui
+            WHEN shohin_bunrui = 'キッチン用品' THEN 'C：' || shohin_bunrui
+            ELSE NULL
+       END AS abc_shohin_bunrui
+  FROM Shohin;

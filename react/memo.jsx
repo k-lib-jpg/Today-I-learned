787ec10d3a5c4771751279eb3a1ac73ffeed33fa
Kyon001 <k-liberty080822@outlook.com>
2026-07-04 22:55:48 +0900

    Update memo.jsx

--- react/memo.jsx
+++ react/memo.jsx
@@ -1,2 +1,24 @@
 //memoとはprops自身が変更されない限り再レンダリングしないようにする仕組みのことである
 //下の例でいうと親コンポーネントの変化によって子コンポーネントも一緒に再レンダリングされるのを防いでいる
+
+export const ChildArea = memo((props) => {
+  const { open, onClickClose } = props;
+  console.log('ChildrenAreaがレンダリングされた');
+
+  const data = [...Array(2000).keys()];
+  data.forEach(() => {
+    console.log('...');
+  });
+
+  return (
+    <>
+      {open ? (
+        <div style={style}>
+          <p>子コンポーネント</p>
+          <button onClick={onClickClose}>閉じる</button>
+        </div>
+      ) : null}
+    </>
+  );
+});
+

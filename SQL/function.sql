0032b35a337382b9f38323dfef4a77574f8be687
Kyon001 <k-liberty080822@outlook.com>
2026-07-12 22:34:06 +0900

    Update function.sql

--- SQL/function.sql
+++ SQL/function.sql
@@ -5,3 +5,11 @@ SELECT COUNT(*)
 /* SUM関数 */
 SELECT SUM(hanbai_tanka)
   FROM Shohin;
+
+/* Min/Max関数 */
+SELECT MIN(hanbai_tanka) 
+FROM Shohin;
+
+/* Avg関数 */
+SELECT AVG(hanbai_tanka) 
+FROM Shohin;

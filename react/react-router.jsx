df1fc07f11786499961ad58340862c2e23f88f93
Kyon001 <k-liberty080822@outlook.com>
2026-07-13 23:14:37 +0900

    Update react-router.jsx

--- react/react-router.jsx
+++ react/react-router.jsx
@@ -13,26 +13,34 @@ createRoot(document.getElementById('root')!).render(
   </BrowserRouter>
 )
 
-// App.jsx
-import React from 'react';
-import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
+// App.jsx (バージョン6以降）
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom';
+import { Home } from './Home';
+import { Page1 } from './Page1';
+import { Page2 } from './Page2';
 
-import Top from './components/Top';
-import Login from './components/Login';
-import TodoList from './components/TodoList';
-
-const App = () => {
+function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route exact path='/' component={Top} />
-        <Route path='/login' component={Login} />
-        <Route path='/todo' component={TodoList} />
-      </Switch>
-      <Link to='/'>Back To Top</Link>
+      <div>
+        <Link to="/">Home</Link>
+        <br />
+        <Link to="/page1">Page1</Link>
+        <br />
+        <Link to="/page2">Page2</Link>
+      </div>
+
+      {/* Switch から Routes に変更 */}
+      <Routes>
+        {/* v6 では element={...} という属性の中にコンポーネントを書きます */}
+        <Route exact path="/" element={<Home />} />
+        <Route path="/page1" element={<Page1 />} />
+        <Route path="/page2" element={<Page2 />} />
+      </Routes>
     </BrowserRouter>
   );
-};
+}
 
 export default App;
+
 

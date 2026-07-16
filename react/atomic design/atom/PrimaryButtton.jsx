2dde23ef2674ad54c11cfe7a74a766b43b362574
Kyon001 <k-liberty080822@outlook.com>
2026-07-17 00:20:43 +0900

    Create PrimaryButton component with styled BaseButton

---
+++ react/atomic design/atom/PrimaryButtton.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import BaseButton from './BaseButton';
+
+const PrimaryButton = (props) => {
+  const { children } = props;
+  return <SButton>{children}</SButton>;
+};
+
+const SButton = styled(BaseButton)`
+  background-color: #40514e;
+`;
+
+export default PrimaryButton;

fc2d600771f4fea2643442f239d403b3ff63e05e
Kyon001 <k-liberty080822@outlook.com>
2026-07-17 00:21:28 +0900

    Add SecondaryButton component with styled BaseButton

---
+++ react/atomic design/atom/SecondaryButton.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+import BaseButton from './BaseButton';
+
+const SecondaryButton = (props) => {
+  const { children } = props;
+  return <SButton>{children}</SButton>;
+};
+
+const SButton = styled(BaseButton)`
+  background-color: #11909e;
+`;
+
+export default SecondaryButton;

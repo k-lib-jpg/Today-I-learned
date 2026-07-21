d0f510f15bfc229d83d8ecc76d5bc1605dfd5466
Kyon001 <k-liberty080822@outlook.com>
2026-07-21 22:07:42 +0900

    Create UserCard component with user details

---
+++ react/atomic design/organism.jsx
@@ -0,0 +1,40 @@
+//organismとは入力フォームやカードといったmolecularが構成されたインターフェイスが見えるものを指す
+import styled from 'styled-components';
+import Card from '../../atoms/card/Card';
+import UserIconWithName from '../../molecules/user/UserIconWithName';
+
+const UserCard = (props) => {
+  const { user } = props;
+  return (
+    <Card>
+      <UserIconWithName image={user.image} name={user.name} />
+      <SDl>
+        <dl>
+          <dt>メール</dt>
+          <dd>{user.email}</dd>
+          <dt>TEL</dt>
+          <dd>{user.phone}</dd>
+          <dt>会社名</dt>
+          <dd>{user.company.name}</dd>
+          <dt>WEB</dt>
+          <dd>{user.website}</dd>
+        </dl>
+      </SDl>
+    </Card>
+  );
+};
+
+const SDl = styled.dl`
+  text-align: left;
+  margin-bottom: 0px;
+  dt {
+    float: left;
+  }
+  dd {
+    padding-left:32px;
+    padding-bottom: 8px;
+    overflow-wrap: break-word;
+  }
+  `;
+
+export default UserCard;

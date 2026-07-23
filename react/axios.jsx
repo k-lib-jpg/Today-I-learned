62c02e2f2d27ab052acf0700e9269e3d8c2db7ca
Kyon001 <k-liberty080822@outlook.com>
2026-07-23 21:21:37 +0900

    Create axios.jsx

---
+++ react/axios.jsx
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import './App.css';
+
+function App() {
+  const onClickUsers = () => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/users')
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+  const onClickUser1 = () => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/users?id=1')
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+  return (
+    <>
+      <button onClick={onClickUsers}>users</button>
+      <button onClick={onClickUser1}>id=1のuser</button>
+    </>
+  );
+}
+
+export default App;
